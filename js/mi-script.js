f420c50be33aee1d3c3e935b9ce8990da8e9e19f
reneceron <rene.ceron@tec.mx>
2023-05-12 12:51:00 -0600

    actualización

---
+++ js/mi-script.js
@@ -0,0 +1,22 @@
+var mivariable1 = "cadena de texto 1212";
+
+// let esMayorDeEdad = true;
+
+// if (esMayorDeEdad) {
+//     console.log("Eres mayor de edad y puedes entrar al bar");
+// } else {
+//     console.log(
+//         "Lo siento, tienes que ser mayor de edad para entrar al bar"
+//     );
+// }
+
+let edad = false
+
+if (edad){
+console.log("Si puedes pasar eres mayor de edad")
+} else{
+    console.log("No puedes pasar eres menos de edad")
+}
+
+edad ? console.log("Si puedes pasar eres mayor de edad") : console.log("No puedes pasar eres menos de edad")
+
